686f7d99de66370c256b6f3fe52be2e1e63ea1e1
elimor123 <elimo2@ac.sce.ac.il>
2018-11-25 13:06:23 +0200

    Made another change.

--- Project_Team10/Project_Team10/Test1.cpp
+++ Project_Team10/Project_Team10/Test1.cpp
@@ -8,29 +8,29 @@ void DataFilesWriteOnly(string file) {
 	if (file == "War") {
 		ofstream War;
 		War.open("War.txt");
-		War << "---------- War game Restructions below -----------" << endl;
+		War << "---------- War game Instructions below -----------" << endl;
 		// we need to fill in the battle card restructions here << // 
 		War.close();
 	}
 	if (file == "4InARow") {
 		ofstream Row;
 		Row.open("4 in a row.txt");
-		Row << "---------- 4 in a Row game Restructions below -----------" << endl;
+		Row << "---------- 4 in a Row game Instructions below -----------" << endl;
 		// we need to fill in the 4 in a row restructions below << //
 		Row.close();
 	}
 	if (file == "Snakes") {
 		ofstream Snakes;
 		Snakes.open("Snakes n' trails.txt");
-		Snakes << "---------- Snakes n' trails game Restructions below -----------" << endl;
+		Snakes << "---------- Snakes n' trails game Instructions below -----------" << endl;
 		// we need to fill in the Snakes 'n trails restructions below << //
 		Snakes.close();
 	}
 }
 
-void Restructions() {
+void Instructions() {
 
-	cout << "------------- Choose a game to view restruction game's comment -------------" << endl;
+	cout << "------------- Choose a game to view instruction game's comment -------------" << endl;
 	cout << "1) for card battle fill in [War]." << endl << "2) for 4 in a row fill in [4InARow]." << endl << "3) for snakes n' trails fill in [Snakes]." << endl;
 	string str;
 	cout << "Please select your choise :";
@@ -51,6 +51,6 @@ void DataFilesReadOnly() {
 }
 
 void GuideApply() {
-	Restructions();
+	Instructions();
 	DataFilesReadOnly();
 }
