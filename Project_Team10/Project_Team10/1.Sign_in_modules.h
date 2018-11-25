d8f8b4afdee45ea1d2edc27c5ec521ffda440225
David Kleinerman <davidkl@ac.sce.ac.il>
2018-11-25 15:07:49 +0200

    sign in finalized

--- Project_Team10/Project_Team10/1.Sign_in_modules.h
+++ Project_Team10/Project_Team10/1.Sign_in_modules.h
@@ -1,11 +1,17 @@
 #define CRT_SECURE_NO_WARNINGS
 #define admin_pass 77766613
+
 #include <string>
 #include <iostream>
-#include <fstream> 
+#include <fstream>
+#include "Structs.h"
 using namespace std;
+
+bool sign_in(User& player);
+void create_user(User& p1, User& p2);
+void get_user_data(string& user_name, long int& pass, bool& admin);
 //function for signing in
-bool sign_in() {
+bool sign_in(User& player) {
 	int choice;
 	bool exists = 0, admin = 0, premium = 0, admin_temp, prem_temp;
 	ifstream users_in;
@@ -16,19 +22,18 @@ bool sign_in() {
 	users_in.open("user_data.txt", ios::in);
 	cout << "for sign-in press 1\nfor log-in press 2" << endl;
 	cin >> choice;
-	cout << "Please enter name" << endl;
-	cin >> user_name;
-	cout << "Please enter password" << endl;
-	cin >> pass;
-	if (pass == admin_pass)
-		admin = 1;
-	switch (choice) {
-	case 1:
+	if (choice == 1) {
+		get_user_data(user_name, pass, admin);
 		if (users_in.peek() == std::ifstream::traits_type::eof()) {
 			users_out << admin << "\n";
+			player.if_manager = admin;
 			users_out << premium << "\n";
+			player.if_premium = premium;
 			users_out << user_name << "\n";
+			player.name = user_name;
 			users_out << pass << "\n";
+			player.password = pass;
+			users_in.close();
 			users_out.close();
 			return true;
 		}
@@ -40,27 +45,26 @@ bool sign_in() {
 				users_in >> temp_pass;
 				if (user_name == temp) {
 					cout << "Name already exists!\n";
-					exists = 1;
-					break;
+					users_in.close();
+					users_out.close();
+					return false;
 				}
 			}
-			if (exists == 0) {
-				users_out << admin << "\n";
-				users_out << premium << "\n";
-				users_out << user_name << "\n";
-				users_out << pass << "\n";
-				users_in.close();
-				users_out.close();
-				return true;
-			}
-			else {
-				users_in.close();
-				users_out.close();
-				return false;
-			}
+			users_out << admin << "\n";
+			player.if_manager = admin;
+			users_out << premium << "\n";
+			player.if_premium = premium;
+			users_out << user_name << "\n";
+			player.name = user_name;
+			users_out << pass << "\n";
+			player.password = pass;
+			users_in.close();
+			users_out.close();
+			return true;
 		}
-		break;
-	case 2:
+	}
+	else if (choice == 2) {
+		get_user_data(user_name, pass, admin);
 		if (users_in.peek() == std::ifstream::traits_type::eof()) {
 			cout << "User doesn't exist!\n";
 			return false;
@@ -72,28 +76,44 @@ bool sign_in() {
 				users_in >> temp;
 				users_in >> temp_pass;
 				if (user_name == temp && pass == temp_pass) {
-					exists = 1;
-					break;
+					users_in.close();
+					users_out.close();
+					return true;
+				}
+				if (user_name == temp && pass != temp_pass) {
+					cout << "wrong password!" << endl;
+					users_in.close();
+					users_out.close();
+					return false;
 				}
 			}
-			if (exists == 1) {
-				users_in.close();
-				users_out.close();
-				return true;
-			}
-			else {
-				cout << "User already exist!" << endl;
-				users_in.close();
-				users_out.close();
-				return false;
-			}
+			cout << "User doesn't exist!" << endl;
+			users_in.close();
+			users_out.close();
+			return false;
 		}
-		break;
-	default:
+	}
+	else {
 		cout << "Wrong input!" << endl;
 		users_in.close();
 		users_out.close();
 		return false;
-		break;
 	}
+
+}
+
+void create_user(User& p1, User& p2) {
+	cout << "signing player 1 " << endl;
+	while (!sign_in(p1)) {}
+	cout << "signing player 2 " << endl;
+	while (!sign_in(p2)) {}
+}
+
+void get_user_data(string& user_name, long int& pass, bool& admin) {
+	cout << "Please enter name" << endl;
+	cin >> user_name;
+	cout << "Please enter password" << endl;
+	cin >> pass;
+	if (pass == admin_pass)
+		admin = 1;
 }
