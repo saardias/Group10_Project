e54e79b99d04de4e9953f3759bb10a91b32eb3a0
סער דיאס <saardi@ac.sce.ac.il>
2018-12-13 14:40:29 +0200

    change in connect four

--- Project_Team10/Project_Team10/4inaRow.h
+++ Project_Team10/Project_Team10/4inaRow.h
@@ -14,13 +14,19 @@ using namespace std;
 #define _CRT_SECURE_NO_WARNINGS
 
 typedef struct {
-	int len=Mlen, width=Mwidth; // size
+	int len = Mlen, width = Mwidth; // size
 	char **player; // table view
 }Board;
 /*void print(Board *B); // printing function
 int PlacePlayer(char p, int x,Board *B); // spotting player function
 bool Winner(int x, int y, char p,Board *B); // winner boolean function
 */
+void Winner_point(User Winner, User losser, int passed_turns)
+{
+	int winner_points = 0;
+	winner_points = 1000 - (25 * passed_turns);
+	cout << "with " << winner_points << " points" << endl;
+}
 
 void print(Board *B) {
 
@@ -64,62 +70,126 @@ int PlacePlayer(char p, int x, Board *B) {
 	}
 }
 
-bool Winner(int x, int y, char p, Board *B) {
+bool Winner(int x, int y, char p, Board *B, User &player1, User &player2, int passed_turns) {
 	int ccol = 1, crow = 1, cld = 1, crd = 1;
 	for (int i = x, j = y - 1; i < B->len && B->player[i][j] == p; i++) {
 		ccol++;
 		if (ccol >= Win) {
-			cout << endl << ((p == r) ? " Red Wins! " : " Blue Wins!") << endl;
+			if (p == r)
+			{
+				cout << player1.name << "Won ! " << endl;
+				Winner_point(player1, player2, passed_turns);
+			}
+			else
+			{
+				cout << player2.name << "Won ! " << endl;
+				Winner_point(player2, player1, passed_turns);
+			}
 			return true;
 		}
 	} // this loop checking win rate between blue and red on the columns
 	for (int j = y - 2, i = x - 1; j >= 0 && B->player[i][j] == p; j--) {
 		crow++;
 		if (crow >= Win) {
-			cout << endl << ((p == r) ? " Red Wins! " : " Blue Wins!") << endl;
+			if (p == r)
+			{
+				cout << player1.name << "Won ! " << endl;
+				Winner_point(player1, player2, passed_turns);
+			}
+			else
+			{
+				cout << player2.name << "Won ! " << endl;
+				Winner_point(player2, player1, passed_turns);
+			}
 			return true;
 		}
 	} // this loop checking win rate between blue and red from the last spot to the left side on the row
 	for (int j = y, i = x - 1; j < B->width && B->player[i][j] == p; j++) {
 		crow++;
 		if (crow >= Win) {
-			cout << endl << ((p == r) ? " Red Wins! " : " Blue Wins!") << endl;
+			if (p == r)
+			{
+				cout << player1.name << "Won ! " << endl;
+				Winner_point(player1, player2, passed_turns);
+			}
+			else
+			{
+				cout << player2.name << "Won ! " << endl;
+				Winner_point(player2, player1, passed_turns);
+			}
 			return true;
 		}
 	} // this loop checking win rate between blue and red from the last spot to the right side on the row
 	for (int j = y - 2, i = x; i < B->len && B->player[i][j] == p; i++, j--) {
 		cld++;
 		if (cld >= Win) {
-			cout << endl << ((p == r) ? " Red Wins! " : " Blue Wins!") << endl;
+			if (p == r)
+			{
+				cout << player1.name << "Won ! " << endl;
+				Winner_point(player1, player2, passed_turns);
+			}
+			else
+			{
+				cout << player2.name << "Won ! " << endl;
+				Winner_point(player2, player1, passed_turns);
+			}
 			return true;
 		}
 	} // this loop checking win rate between blue and red from the last spot to the left diagonal on the board
 	for (int j = y, i = x; i < B->len && B->player[i][j] == p; i++, j++) {
 		crd++;
 		if (crd >= Win) {
-			cout << endl << ((p == r) ? " Red Wins! " : " Blue Wins!") << endl;
+			if (p == r)
+			{
+				cout << player1.name << "Won ! " << endl;
+				Winner_point(player1, player2, passed_turns);
+			}
+			else
+			{
+				cout << player2.name << "Won ! " << endl;
+				Winner_point(player2, player1, passed_turns);
+			}
 			return true;
 		}
 	} // this loop checking win rate between blue and red from the last spot to the right diagonal on the board
 	for (int j = y, i = x - 2; i >= 0 && j < B->width && B->player[i][j] == p; i--, j++) {
 		cld++;
 		if (cld >= Win) {
-			cout << endl << ((p == r) ? " Red Wins! " : " Blue Wins!") << endl;
+			if (p == r)
+			{
+				cout << player1.name << "Won ! " << endl;
+				Winner_point(player1, player2, passed_turns);
+			}
+			else
+			{
+				cout << player2.name << "Won ! " << endl;
+				Winner_point(player2, player1, passed_turns);
+			}
 			return true;
 		}
 	} // this loop checking win rate between blue and red from the last spot on the upper left diagonal
 	for (int j = y - 2, i = x - 2; i >= 0 && j >= 0 && B->player[i][j] == p; i--, j--) {
 		crd++;
-		if (crd == Win) {
-			cout << endl << ((p == r) ? " Red Wins! " : " Blue Wins!") << endl;
+		if (crd >= Win) {
+			if (p == r)
+			{
+				cout << player1.name << "Won ! " << endl;
+				Winner_point(player1, player2, passed_turns);
+			}
+			else
+			{
+				cout << player2.name << "Won ! " << endl;
+				Winner_point(player2, player1, passed_turns);
+			}
 			return true;
 		}
 	} // this loop checking win rate between blue and red from the last spot on the upper right diagonal
 	return false;
 }
 
-void PlayForInARow() {
+void PlayForInARow(User &player1, User &player2) {
 	Board *P = new Board;
+	int passed_turns = 0;
 	int k = 0, i, placement, temp, row, col;
 	row = P->len;
 	col = P->width;
@@ -133,7 +203,7 @@ void PlayForInARow() {
 	print(P);
 	for (i = 0; i < row*col; i++) {
 		if (k % 2 == 0) {
-			cout << "Please enter your input! [1-" << col << "]" << " Player (Red) :";
+			cout << player1.name << "Please enter your input! [1-" << col << "]" << " Player (Red) :";
 			cin >> placement;
 			if (placement > col || placement <= 0) {
 				cout << " Board-size deviation! Please re-enter your input " << endl;
@@ -141,12 +211,12 @@ void PlayForInARow() {
 			}
 			temp = PlacePlayer(r, placement, P);
 			print(P);
-			if (i >= 6 && temp > 0 && Winner(temp, placement, r, P) == true) {
+			if (i >= 6 && temp > 0 && Winner(temp, placement, r, P, player1, player2, passed_turns) == true) {
 				break;
 			}
 		}
 		if (k % 2 != 0) {
-			cout << "Please enter your input! [1-" << col << "]" << " Player (Blue) :";
+			cout << player2.name << "Please enter your input! [1-" << col << "]" << " Player (Blue) :";
 			cin >> placement;
 			if (placement > col || placement <= 0) {
 				cout << " Board-size deviation! Please re-enter your input " << endl;
@@ -154,9 +224,10 @@ void PlayForInARow() {
 			}
 			temp = PlacePlayer(b, placement, P);
 			print(P);
-			if (i >= 6 && temp > 0 && Winner(temp, placement, b, P) == true) {
+			if (i >= 6 && temp > 0 && Winner(temp, placement, b, P, player1, player2, passed_turns) == true) {
 				break;
 			}
+			passed_turns++;
 		}
 		k++;
 	} // this loop running the Connect Four game
