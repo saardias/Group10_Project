af95c26445d4387e821c91b5c8fe6b6dc407dbf5
nitay malka <nitaai.maalka@gmail.com>
2018-12-24 11:56:17 +0200

    pause op and input cheks for war game

--- Project_Team10/Project_Team10/Header1.h
+++ Project_Team10/Project_Team10/Header1.h
@@ -24,26 +24,27 @@ int Random() {
 void PrintPack(CardPackage pack) {//for tests
 	/*print all cards in package*/
 	for (int i = 0; i < 13; i++) {
-		cout << pack.Card_Package[i].num_of_card << " num of type " << pack.Card_Package[i].card_name << endl;
+		std::cout << pack.Card_Package[i].num_of_card << " num of type " << pack.Card_Package[i].card_name << endl;
 	}
 }
 void OneRoundWinner(User &win, User &looser) {
-	cout << "winner of this round is: " << win.name << endl;
-	cout << looser.name << " you such a looser go home" << endl;
+	std::cout << "winner of this round is: " << win.name << endl;
+	std::cout << looser.name << " you such a looser go home" << endl;
 }
 void WarGameWinner(User &win, User &looser, int winner_points, int looser_points) {
-	cout << "\n\n\n========- GAME OVER -========" << endl;
+	std::cout << "\n\n\n========- GAME OVER -========" << endl;
 
-	cout << "\n\n and the big winner of the game is: " << win.name << endl;
-	cout << " winner points: " << winner_points << endl;
-	cout << looser.name << " go home you are stupid" << endl;
-	cout << " looser points: " << looser_points << endl;
-	cout << "====================================" << endl;
+	std::cout << "\n\n and the big winner of the game is: " << win.name << endl;
+	std::cout << " winner points: " << winner_points << endl;
+	std::cout << looser.name << " go home you are stupid" << endl;
+	std::cout << " looser points: " << looser_points << endl;
+	std::cout << "====================================" << endl;
 
 	Set_Cards_Battle_Statistics(win.name, looser.name);
 
-	cout << "press enter to exit" << endl;
+	std::cout << "press enter to exit" << endl;
 	cin.ignore(std::numeric_limits<streamsize>::max(), '\n');
+
 
 }
 void PlayWar(User &U1, User &U2, int num_of_turns = 26) {
@@ -60,24 +61,33 @@ void PlayWar(User &U1, User &U2, int num_of_turns = 26) {
 		pack.pack_size += 4;// unnesessery we know its 52 in the end
 	}
 	// the main player choose to play against computer or against user2
-	char choice = 'u';
+	string choice = "u";
 	string tmp_u2_name;
 	bool u2_is_computer;
 	do {
-		cout << "to play against " << U2.name << " press 1" << endl;
-		cout << "to play against computer press 2" << endl;
+		std::cout << "to play against " << U2.name << " press 1" << endl;
+		std::cout << "to play against computer press 2" << endl;
 		cin >> choice;
-	} while (choice != '1'&&choice != '2');
-	if (choice == '1') { u2_is_computer = false; }
-	if (choice == '2') { u2_is_computer = true; tmp_u2_name = U2.name; U2.name = "watson"; }
+	} while (choice != "1"&&choice != "2");
+	if (choice == "1") { u2_is_computer = false; }
+	if (choice == "2") { u2_is_computer = true; tmp_u2_name = U2.name; U2.name = "computer"; }
 
 	/*********************game start here**************************/
 	for (int ctr_turns = 0; ctr_turns < num_of_turns; ctr_turns++) {//
 		/******************START OF U1 TURN***********************************/
-		cout << "\n\nround number " << ctr_turns + 1 << "\nits " << U1.name << " turn" << endl;
+		std::cout << "\n\nround number " << ctr_turns + 1 << "\nits " << U1.name << " turn" << endl;
 		//U1 pull out card
-		cout << "press enter to draw card" << endl;
-		cin.ignore(std::numeric_limits<streamsize>::max(), '\n');
+		
+				std::cout << "press 1 to pause\nor any other key to draw card" << endl;
+				cin >> choice;
+				if(choice=="1"){
+				cout << "press E to exit\nor any other key to continue\n";
+				cin >> choice;
+				if(choice=="E"||choice=="e"){return;}
+				}
+		
+		//std::cout << "press enter to draw card" << endl;
+		//cin.ignore(std::numeric_limits<streamsize>::max(), '\n');
 		tmp = Random();
 		while (pack.Card_Package[tmp].num_of_card < 1 || pack.Card_Package[tmp].num_of_card>4)
 		{
@@ -88,13 +98,13 @@ void PlayWar(User &U1, User &U2, int num_of_turns = 26) {
 		}
 		u1_card.card_name = pack.Card_Package[tmp].card_name;
 		if (U1.if_premium) {
-			cout << "\n\n" << U1.name << " you are a premium member \n do you want to draw another card?" << endl;
-			cout << "it will cost you "<<ctr_p_changes<<" points" << endl;
-			cout << "press 1 to draw card" << endl;
-			cout << "press 2 to pass" << endl;
-			int choice;
+			std::cout << "\n\n" << U1.name << " you are a premium member \n do you want to draw another card?" << endl;
+			std::cout << "you can do it " << ctr_p_changes << " times in this game" << endl;
+			std::cout << "press 1 to draw card" << endl;
+			std::cout << "press 2 to pass" << endl;
+	//		int choice;
 			cin >> choice;
-			if (choice == 1) {
+			if (choice == "1") {
 				//U1 pull out card
 				ctr_p_changes--;
 				tmp = Random();
@@ -112,17 +122,26 @@ void PlayWar(User &U1, User &U2, int num_of_turns = 26) {
 		u1_card.num_of_card = pack.Card_Package[tmp].num_of_card;
 		u1_sum += u1_card.card_name;
 		//ifpremium-need to do
-		cout << U1.name << " card is:";
+		std::cout << U1.name << " card is:";
 		PrintCard(u1_card.card_name);
 		/******************END OF U1 TURN***********************************/
 
 		/******************START OF U2 TURN***********************************/
-		cout << "now its " << U2.name << " turn" << endl;
+		std::cout << "now its " << U2.name << " turn" << endl;
 		//U2 pull out card
 		if (!u2_is_computer) {
-			cout << "press enter to draw card" << endl;
-			cin.ignore(std::numeric_limits<streamsize>::max(), '\n');
+		
+		std::cout << "press 1 to pause\nor any other key to draw card" << endl;
+		cin>>choice;
+		if(choice=="1"){
+		cout<<"press E to exit\nor any other key to continue\n";
+		cin>>choice;
+		if(choice=="E"||choice=="e"){return;}
 		}
+		}
+		
+			std::cout << "press enter to draw card" << endl;
+			cin.ignore(std::numeric_limits<streamsize>::max(), '\n');
 		tmp = Random();
 		if(tmp%3==0){ tmp +=3; }
 		if (tmp % 3 == 1) { tmp -= 2; }
@@ -136,14 +155,14 @@ void PlayWar(User &U1, User &U2, int num_of_turns = 26) {
 			tmp = tmp % 13;
 		}
 		u2_card.card_name = pack.Card_Package[tmp].card_name;
-		if (U2.if_premium) {
-			cout << "\n\n" << U2.name << " you are a premium member \n do you want to draw another card?" << endl;
-			cout << "you can do it " << ctr_p_changes << " times in this game" << endl;
-			cout << "press 1 to draw card" << endl;
-			cout << "press 2 to pass" << endl;
-			int choice;
+		if (U2.if_premium&& !u2_is_computer) {
+			std::cout << "\n\n" << U2.name << " you are a premium member \n do you want to draw another card?" << endl;
+			std::cout << "you can do it " << ctr_p_changes << " times in this game" << endl;
+			std::cout << "press 1 to draw card" << endl;
+			std::cout << "press 2 to pass" << endl;
+			//int choice;
 			cin >> choice;
-			if (choice == 1) {
+			if (choice == "1") {
 				//U1 pull out card
 				ctr_p_changes--;
 				tmp = Random();
@@ -161,12 +180,12 @@ void PlayWar(User &U1, User &U2, int num_of_turns = 26) {
 		pack.Card_Package[u2_card.card_name-2].num_of_card--;
 		u2_card.num_of_card = pack.Card_Package[tmp].num_of_card;
 		u2_sum += u2_card.card_name;
-		cout << U2.name << " card is:" << endl;
+		std::cout << U2.name << " card is:" << endl;
 		PrintCard(u2_card.card_name);
 		/******************END OF U2 TURN***********************************/
-		cout << endl;
+		std::cout << endl;
 		/************* BATTLE BEGIN ****************/
-		cout << "		/************* BATTLE BEGIN ****************/" << endl;
+		std::cout << "		/************* BATTLE BEGIN ****************/" << endl;
 		if (u1_card.card_name > u2_card.card_name) {
 			OneRoundWinner(U1, U2);
 			u1_wins_ctr++;
@@ -176,8 +195,8 @@ void PlayWar(User &U1, User &U2, int num_of_turns = 26) {
 			u2_wins_ctr++;
 		}
 		else { //war state
-			cout << U1.name << " have " << u1_sum << " points in the pocket" << endl;
-			cout << U2.name << " have " << u2_sum << " points in the pocket" << endl;
+			std::cout << U1.name << " have " << u1_sum << " points in the pocket" << endl << endl;
+			std::cout << U2.name << " have " << u2_sum << " points in the pocket" << endl;
 			if (u1_sum > u2_sum) {
 				OneRoundWinner(U1, U2);
 
@@ -188,9 +207,9 @@ void PlayWar(User &U1, User &U2, int num_of_turns = 26) {
 				u2_wins_ctr++;
 			}
 			if (u1_sum == u2_sum) {
-				cout << "no one win in this battle" << endl;//rare after beggining
+				std::cout << "no one win in this battle" << endl;//rare after beggining
 			}
-			cout << "		/************* BATTLE END ****************/" << endl;
+			std::cout << "		/************* BATTLE END ****************/" << endl;
 
 			/************* BATTLE END ****************/
 
@@ -199,7 +218,7 @@ void PlayWar(User &U1, User &U2, int num_of_turns = 26) {
 	/*****WarGameWinner******/
 	if (u1_wins_ctr > u2_wins_ctr) { WarGameWinner(U1, U2, u1_wins_ctr, u2_wins_ctr); if (U2.if_premium) { Pre_Lost(U2, U1); } }
 	if (u1_wins_ctr < u2_wins_ctr) { WarGameWinner(U2, U1, u2_wins_ctr, u1_wins_ctr); if (U1.if_premium) { Pre_Lost(U1, U2); } }
-	if (u1_wins_ctr == u2_wins_ctr) { cout << "both of you are loosers" << endl; }//rare 
+	if (u1_wins_ctr == u2_wins_ctr) { std::cout << "its a tie, no one win" << endl; }//rare 
 
 	if (u2_is_computer) { U2.name = tmp_u2_name; }
 }
